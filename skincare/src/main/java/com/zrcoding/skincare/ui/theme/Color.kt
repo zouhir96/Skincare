4e430487c500252d118abcd27e81c04f89a5df46
zouhir <rajdaouizouhir.pro@gmail.com>
2022-12-21 20:01:06 +0100

    upgrade dependencies + setup app theme

--- skincare/src/main/java/com/zrcoding/skincare/ui/theme/Color.kt
+++ skincare/src/main/java/com/zrcoding/skincare/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val brouwn = Color(0xFF483434)
+val brouwnWhite80 = Color(0xFFFFF3E4)
+val brouwnWhite50 = Color(0xFFD0C9C0)
+val brouwnWhite30 = Color(0xFFFFF3E4)
--- skincare/src/main/java/com/zrcoding/skincare/ui/theme/Type.kt
+++ skincare/src/main/java/com/zrcoding/skincare/ui/theme/Type.kt
@@ -1,34 +1,49 @@
 package com.zrcoding.skincare.ui.theme
 
-import androidx.compose.material3.Typography
+import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.zrcoding.skincare.R
+
+val SOFIA_SANS = FontFamily(
+    Font(resId = R.font.sofia_sans_medium),
+    Font(resId = R.font.sofia_sans_medium, FontWeight.W600),
+    Font(resId = R.font.sofia_sans_medium, FontWeight.W500)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+    subtitle1 = TextStyle(
+        fontFamily = SOFIA_SANS,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        fontSize = 16.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    subtitle2 = TextStyle(
+        fontFamily = SOFIA_SANS,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = SOFIA_SANS,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    ),
+    body2 = TextStyle(
+        fontFamily = SOFIA_SANS,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    button = TextStyle(
+        fontFamily = SOFIA_SANS,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = SOFIA_SANS,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
     )
-    */
 )
