ea53ab0a31ec98f025c13bbb5eb8d447d4321220
zouhir <rajdaouizouhir.pro@gmail.com>
2022-12-26 19:41:06 +0100

    refactor: added some code refactoring

--- skincare/src/main/java/com/zrcoding/skincare/ui/MainActivity.kt
+++ skincare/src/main/java/com/zrcoding/skincare/ui/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.zrcoding.skincare
+package com.zrcoding.skincare.ui
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -24,6 +24,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.zrcoding.skincare.R
 import com.zrcoding.skincare.ui.cart.Cart
 import com.zrcoding.skincare.ui.explore.Explore
 import com.zrcoding.skincare.ui.favorite.Favorites
