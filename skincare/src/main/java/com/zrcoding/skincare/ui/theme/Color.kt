d98343b81d6f8aaf2116067f158a0b6a355da475
zouhir <rajdaouizouhir.pro@gmail.com>
2023-01-09 19:47:17 +0100

    feat: add toolbar with dynamic cart items count

--- skincare/src/main/java/com/zrcoding/skincare/ui/theme/Color.kt
+++ skincare/src/main/java/com/zrcoding/skincare/ui/theme/Color.kt
@@ -3,6 +3,7 @@ package com.zrcoding.skincare.ui.theme
 import androidx.compose.ui.graphics.Color
 
 val White = Color(0xFFFFFFFF)
+val Lotion = Color(0xFFFAFAFA)
 
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
@@ -11,14 +12,16 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
-
 val Brown = Color(0xFF483434)
+
 val Brown80 = Color(0xFF6B4F4F)
 val BrownWhite90 = Color(0xFFDEDEDE)
 val BrownWhite80 = Color(0xFFFFF3E4)
 val BrownWhite50 = Color(0xFFD0C9C0)
 val BrownWhite30 = Color(0xFFFFF3E4)
-
 val Grey = Color(0xFFBCBCBC)
+
 val Grey30 = Color(0xFFAAA492)
 val Grey40 = Color(0xFFA6A6A6)
+
+val CoralReef = Color(0xFFFF7878)
