59369eacfda2a66ce6eb148d80e5c980fbc4a1b0
zouhir <rajdaouizouhir.pro@gmail.com>
2023-05-05 20:15:11 +0100

    feature: Add more methods to repositories.

--- data/src/main/kotlin/com/zrcoding/skincare/data/domain/repositories/ProductRepository.kt
+++ data/src/main/kotlin/com/zrcoding/skincare/data/domain/repositories/ProductRepository.kt
@@ -7,4 +7,18 @@ interface ProductRepository {
     fun getAll(): Flow<List<Product>>
 
     suspend fun getOne(uuid: String): Product?
+
+    suspend fun searchProduct(searchText: String): List<Product>
+
+    suspend fun searchProduct(searchText: String, limit: Int): List<Product>
+
+    suspend fun getCategoryProducts(categoryUuid: String): List<Product>
+
+    suspend fun searchCategoryProducts(searchText: String, categoryUuid: String): List<Product>
+
+    suspend fun getMostPopularProducts(limit: Int): List<Product>
+
+    suspend fun getMostPopularProducts(limit: Int, categoryUuid: String): List<Product>
+
+    suspend fun getNewestProduct(): Product?
 }
--- data/src/mock/kotlin/om/zrcoding/skincare/data/repositories/CategoryRepositoryMockSuccess.kt
+++ data/src/mock/kotlin/om/zrcoding/skincare/data/repositories/CategoryRepositoryMockSuccess.kt
@@ -2,13 +2,12 @@ package om.zrcoding.skincare.data.repositories
 
 import com.zrcoding.skincare.data.domain.model.Category
 import com.zrcoding.skincare.data.domain.repositories.CategoryRepository
-import kotlinx.coroutines.flow.flowOf
 import java.util.UUID
 import javax.inject.Inject
 
 class CategoryRepositoryMockSuccess @Inject constructor() : CategoryRepository {
 
-    override fun getAll() = flowOf(fakeCategories)
+    override suspend fun getAll() = fakeCategories
 
     override suspend fun getOne(uuid: String) = fakeCategories.find { it.uuid == uuid }
 }
